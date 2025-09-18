d3a848c3a5e0afb12e23416cf6c0084efb273d73
Kirpich Mira <k.maltsev@g.nsu.ru>
2025-09-18 23:43:01 +0700

    Ya ebal

---
+++ Task_1_1_1/docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.maltsev","l":"Heapsort"},{"p":"ru.nsu.maltsev","l":"Main"}];updateSearchResults();
---
+++ Task_1_1_1/src/main/java/ru/nsu/maltsev/Main.java
@@ -0,0 +1,10 @@
+package ru.nsu.maltsev;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 3, 2, 1};
+        Heapsort.heapsort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
