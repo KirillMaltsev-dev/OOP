9e44b320e77cb035d2b2209e5e85e086cf1d7bf7
Kirpich Mira <k.maltsev@g.nsu.ru>
2025-10-16 19:13:42 +0700

    Task_1_1_3

---
+++ Task_1_1_3/src/main/java/ru/nsu/maltsev/Task_1_1_3/Mul.java
@@ -0,0 +1,55 @@
+package ru.nsu.maltsev.Task_1_1_3;
+
+import java.util.Map;
+
+public class Mul extends Expression{
+    private final Expression left;
+    private final Expression right;
+
+    public Mul(Expression left, Expression right){
+        this.left = left;
+        this.right = right;
+    }
+
+    public int eval(Map<String, Integer> vars){
+        return left.eval(vars) * right.eval(vars);
+    }
+
+    public Expression derivative(String var){
+        return new Add(new Mul(left.derivative(var), right), new Mul(right.derivative(var), left));
+    }
+
+    public void print(){
+        System.out.print("(");
+        left.print();
+        System.out.print("*");
+        right.print();
+        System.out.print(")");
+    }
+
+    public Expression simple(){
+        Expression l = left.simple();
+        Expression r = right.simple();
+
+        if (l instanceof Number && r instanceof Number) {
+            return new Number(((Number) l).getValue() * ((Number) r).getValue());
+        }
+
+        if (l instanceof Number && ((Number) l).getValue() == 0) return new Number(0);
+        if (r instanceof Number && ((Number) r).getValue() == 0) return new Number(0);
+
+        if (l instanceof Number && ((Number) l).getValue() == 1) return r;
+        if (r instanceof Number && ((Number) r).getValue() == 1) return l;
+
+        return new Mul(l, r);
+    }
+
+    public boolean structuralEquals(Expression other) {
+        if (!(other instanceof Mul)) {
+            return false;
+        }
+        Mul otherMul = (Mul) other;
+        return this.left.structuralEquals(otherMul.left) &&
+                this.right.structuralEquals(otherMul.right);
+    }
+}
---
+++ Task_1_1_3/src/main/java/ru/nsu/maltsev/Task_1_1_3/Variable.java
@@ -0,0 +1,45 @@
+package ru.nsu.maltsev.Task_1_1_3;
+
+import java.util.Map;
+
+public class Variable extends Expression{
+    private final String name;
+
+    public Variable(String name){
+        this.name = name;
+    }
+
+    public int eval(Map <String, Integer> vars){
+        if (!vars.containsKey(name)) {
+            throw new IllegalArgumentException("Variable not defined: " + name);
+        }
+        return vars.get(name);
+    }
+
+    public Expression derivative(String var){
+        if (name.equals(var)){
+            return new Number(1);
+        }
+        else{
+            return new Number(0);
+        }
+    }
+    public void print(){
+        System.out.print(name);
+    }
+
+    public Expression simple(){
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean structuralEquals(Expression other) {
+        if (!(other instanceof Variable)) {
+            return false;
+        }
+        return this.name.equals(((Variable) other).name);
+    }
+}
