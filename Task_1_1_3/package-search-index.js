ee484efdbe81403e77e89e5b664ee8c9e916b9b2
KirillMaltsev-dev <KirillMaltsev-dev@users.noreply.github.com>
2025-10-20 04:15:32 +0000

    Deploying to gh-pages from @ KirillMaltsev-dev/OOP@50d788b4e1603bfe89ee0319992a1a673ed10949 🚀

--- Task_1_1_3/package-search-index.js
+++ Task_1_1_3/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.maltsev.Task_1_1_3"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.maltsev.task3"}];updateSearchResults();
