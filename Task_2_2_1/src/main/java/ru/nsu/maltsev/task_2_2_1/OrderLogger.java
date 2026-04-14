8cf6bebc782f075c9a4ef2fc1bbcd9c67331fc20
Kirpich Mira <k.maltsev@g.nsu.ru>
2026-04-14 11:48:46 +0700

    logger changed

--- Task_2_2_1/src/main/java/ru/nsu/maltsev/task_2_2_1/OrderLogger.java
+++ Task_2_2_1/src/main/java/ru/nsu/maltsev/task_2_2_1/OrderLogger.java
@@ -1,10 +1,15 @@
 package ru.nsu.maltsev.task_2_2_1;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public final class OrderLogger {
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderLogger.class);
+
     private OrderLogger() {
     }
 
-    public static synchronized void log(int orderId, OrderState state) {
-        System.out.println(orderId + " " + state.name());
+    public static void log(int orderId, OrderState state) {
+        LOGGER.info("{} {}", orderId, state.name());
     }
 }
